42b305e4791a07ff02e90af40b2ecfcc940b3660
sgrfazli <1rezartabriz@gmail.com>
2022-10-25 11:21:17 +0100

    add testing features

--- Neural_Networks.cpp
+++ Neural_Networks.cpp
@@ -43,7 +43,7 @@ int main() {
     MultiLayerPerceptron mlp = MultiLayerPerceptron({2, 2, 1});
     cout << "Traing Neural Network as and XOR Gate ...\n";
     double MSE;
-    for (int i = 0; i < 30000; i++) {
+    for (int i = 0; i < 3000; i++) {
         MSE = 0.0;
         MSE += mlp.bp({0, 0}, {0});
         MSE += mlp.bp({0, 1}, {1});
@@ -62,5 +62,74 @@ int main() {
     cout <<"0 1 = "<< mlp.run({0,1})[0]<< endl;
     cout <<"1 0 = "<< mlp.run({1,0})[0]<< endl;
     cout <<"1 1 = "<< mlp.run({1,1})[0]<< endl;
+
+
+   // Test code - Segment Display Recognition System
+   int epochs = 1000;
+   MultiLayerPerceptron *sdrnn;
+
+   sdrnn = new MultiLayerPerceptron({7,7,1});
+
+   // Dataset for the 7 to 1 network
+   for (int i=0; i<epochs; i++) {
+       MSE = 0.0;
+       MSE += sdrnn->bp({1, 1, 1, 1, 1, 1, 0}, {0.05}); //0 pattern
+       MSE += sdrnn->bp({0, 1, 1, 0, 0, 0, 0}, {0.15}); //1 pattern
+       MSE += sdrnn->bp({1, 1, 0, 1, 1, 0, 1}, {0.25}); //2 pattern
+       MSE += sdrnn->bp({1, 1, 1, 1, 0, 0, 1}, {0.35}); //3 pattern
+       MSE += sdrnn->bp({0, 1, 1, 0, 0, 1, 1}, {0.45}); //4 pattern
+       MSE += sdrnn->bp({1, 0, 1, 1, 0, 1, 1}, {0.55}); //5 pattern
+       MSE += sdrnn->bp({1, 0, 1, 1, 1, 1, 1}, {0.65}); //6 pattern
+       MSE += sdrnn->bp({1, 1, 1, 0, 0, 0, 0}, {0.75}); //7 pattern
+       MSE += sdrnn->bp({1, 1, 1, 1, 1, 1, 1}, {0.85}); //8 pattern
+       MSE += sdrnn->bp({1, 1, 1, 1, 0, 1, 1}, {0.95}); //9 pattern
+   }
+   MSE/= 10.0;
+   cout<< endl << "7 to 1  network MSE: " << MSE << endl;
+
+   sdrnn->print_weight();
+    delete(sdrnn);
+
+//     Dataset for the 7 to 10 network
+    sdrnn = new MultiLayerPerceptron({7,7,10});
+    MSE = 0.0;
+    for (int i=0;i<epochs; i++){
+        MSE = 0.0;
+        MSE += sdrnn->bp({1,1,1,1,1,1,0}, {1,0,0,0,0,0,0,0,0,0}); //0 pattern
+        MSE += sdrnn->bp({0,1,1,0,0,0,0}, {0,1,0,0,0,0,0,0,0,0}); //1 pattern
+        MSE += sdrnn->bp({1,1,0,1,1,0,1}, {0,0,1,0,0,0,0,0,0,0}); //2 pattern
+        MSE += sdrnn->bp({1,1,1,1,0,0,1}, {0,0,0,1,0,0,0,0,0,0}); //3 pattern
+        MSE += sdrnn->bp({0,1,1,0,0,1,1}, {0,0,0,0,1,0,0,0,0,0}); //4 pattern
+        MSE += sdrnn->bp({1,0,1,1,0,1,1}, {0,0,0,0,0,1,0,0,0,0}); //5 pattern
+        MSE += sdrnn->bp({1,0,1,1,1,1,1}, {0,0,0,0,0,0,1,0,0,0}); //6 pattern
+        MSE += sdrnn->bp({1,1,1,0,0,0,0}, {0,0,0,0,0,0,0,1,0,0}); //7 pattern
+        MSE += sdrnn->bp({1,1,1,1,1,1,1}, {0,0,0,0,0,0,0,0,1,0}); //8 pattern
+        MSE += sdrnn->bp({1,1,1,1,0,1,1}, {0,0,0,0,0,0,0,0,0,1}); //9 pattern
+    }
+    MSE /=10.0;
+    cout << "7 to 10 network MSE: " << MSE << endl;
+    sdrnn->print_weight();
+    delete(sdrnn);
+
+    // Dataset for the 7 to 7 network
+    delete(sdrnn);
+    sdrnn = new MultiLayerPerceptron({7,7,7});
+
+    for (int i = 0; i < epochs; i++){
+        MSE = 0.0;
+        MSE += sdrnn->bp({1,1,1,1,1,1,0}, {1,1,1,1,1,1,0}); //0 pattern
+        MSE += sdrnn->bp({0,1,1,0,0,0,0}, {0,1,1,0,0,0,0}); //1 pattern
+        MSE += sdrnn->bp({1,1,0,1,1,0,1}, {1,1,0,1,1,0,1}); //2 pattern
+        MSE += sdrnn->bp({1,1,1,1,0,0,1}, {1,1,1,1,0,0,1}); //3 pattern
+        MSE += sdrnn->bp({0,1,1,0,0,1,1}, {0,1,1,0,0,1,1}); //4 pattern
+        MSE += sdrnn->bp({1,0,1,1,0,1,1}, {1,0,1,1,0,1,1}); //5 pattern
+        MSE += sdrnn->bp({1,0,1,1,1,1,1}, {1,0,1,1,1,1,1}); //6 pattern
+        MSE += sdrnn->bp({1,1,1,0,0,0,0}, {1,1,1,0,0,0,0}); //7 pattern
+        MSE += sdrnn->bp({1,1,1,1,1,1,1}, {1,1,1,1,1,1,1}); //8 pattern
+        MSE += sdrnn->bp({1,1,1,1,0,1,1}, {1,1,1,1,0,1,1}); //9 pattern
+    }
+    MSE /= 10.0;
+    cout << "7 to 7  network MSE: " << MSE << endl << endl;
+
 
 }
